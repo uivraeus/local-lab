c7120bc5515cc08219012bade471352066ad4419
Ulf Ivraeus <ulf.ivraeus@gmail.com>
2025-09-14 16:33:29 +0000

    Add trust-manager and dev-ca bundle to clusters

--- infra/install-cert-manager.sh
+++ infra/install-cert-manager.sh
@@ -12,7 +12,7 @@ kubectl --context $CLUSTER_NAME apply \
   -f https://github.com/cert-manager/cert-manager/releases/download/$CERT_MANAGER_VERSION/cert-manager.crds.yaml
 helm repo add jetstack https://charts.jetstack.io --force-update
 helm --kube-context $CLUSTER_NAME install cert-manager -n cert-manager \
-  --create-namespace --version $CERT_MANAGER_VERSION jetstack/cert-manager
+  --create-namespace --version $CERT_MANAGER_VERSION jetstack/cert-manager --wait
 
 # CA ClusterIssuer
 kubectl --context $CLUSTER_NAME -n cert-manager create secret tls dev-ca \
@@ -25,4 +25,27 @@ metadata:
 spec:
   ca:
     secretName: dev-ca
+EOF
+
+# Also install trust-manager to make it easier to configure trust in cluster-local workloads
+helm --kube-context $CLUSTER_NAME install trust-manager -n cert-manager jetstack/trust-manager --wait
+
+# All namespaces will get a ConfigMap, "my-trusted-ca", with the CA cert, which can be mounted into pods
+cat <<EOF | kubectl --context $CLUSTER_NAME apply -f -
+apiVersion: trust.cert-manager.io/v1alpha1
+kind: Bundle
+metadata:
+  name: my-trusted-ca
+spec:
+  sources:
+  - useDefaultCAs: true
+  - secret:
+      name: "dev-ca"
+      key: "tls.crt"
+  target:
+    configMap:
+      key: "ca-certificates.crt"
+      metadata:
+        labels:
+          app.kubernetes.io/component: "trust-bundle"
 EOF
